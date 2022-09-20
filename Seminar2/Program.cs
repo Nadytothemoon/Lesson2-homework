d63dbbbc7d750d3c9353c598c47b01ff53a24a02
Nady <nadyyakunina@gmail.com>
2022-09-20 21:33:30 +0400

    seminar2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,63 @@
+﻿// int n=new Random().Next (10,100);
+// System.Console.WriteLine(n);
+
+// int d1=n/10;
+// int d2=n%10;
+
+// int max=d1;
+// if (max<d2){
+//     max=d2;
+// }
+// System.Console.WriteLine($"Максимальная цифра числа {n}->{max}");
+
+
+// int n=new Random().Next (100,1000);
+// System.Console.WriteLine(n);
+// int result=n/100*10 + n%10;
+// System.Console.WriteLine($"{n} -> {result}");
+
+// System.Console.Write("Введите число а:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число b:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a%b==0) {
+//     System.Console.WriteLine("кратно");
+// }
+// else {
+//     System.Console.WriteLine($" не кратно, остаток: {a%b}");
+// }
+
+
+// System.Console.Write("Введите число а:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a%7==0) {
+//     if (a%23==0){
+//         System.Console.WriteLine("Делится");
+//     }
+// }
+// else {System.Console.WriteLine("Не делится");
+// }
+
+System.Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a*a==b){
+    System.Console.WriteLine("a является квадратом b ");
+}
+else if (b*b==a){
+    System.Console.WriteLine("b является квадратом a");
+}
+else {
+    System.Console.WriteLine("не является квадратом");
+}
+
+
+
+
+
+
